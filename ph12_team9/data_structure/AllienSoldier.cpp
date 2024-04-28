24a55d0cc9595a4c7c137faeb2cee0b1f89bcc34
Abdallah Ayman <149713232+vAbdallahv@users.noreply.github.com>
2024-04-28 14:54:44 +0300

    edit AS

--- ph12_team9/data_structure/AllienSoldier.cpp
+++ ph12_team9/data_structure/AllienSoldier.cpp
@@ -14,14 +14,16 @@ void AllienSoldier::Attack()
 	EarthArmy* EarthA = game_ptr->get_humans_pointer();
 	armyUnit* ES = new earthSoldier;
 	armyUnit* E = ES;
-	EarthA->deleteUnit(ES);
 	int i = 0;
 	while ((i < this->getAttackCapacity()) && ES)
 	{
-		temp.enqueue(dynamic_cast<earthSoldier*> (ES));
-		ES->set_attacked_time(game_ptr->get_current_time());   //set the first attacked time 
-		ES->set_first_attack_delay();
 		EarthA->deleteUnit(ES);
+		if (ES)
+		{
+			temp.enqueue(dynamic_cast<earthSoldier*> (ES));
+			ES->set_attacked_time(game_ptr->get_current_time());   //set the first attacked time 
+			ES->set_first_attack_delay();
+		}
 		i++;
 	}
 	if (!game_ptr->getSilentMode())
